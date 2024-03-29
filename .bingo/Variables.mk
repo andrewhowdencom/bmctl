bcfddbc80bdda68316ac8f0a64bf92cd9523300a
Andrew Howden <hello@andrewhowden.com>
2024-03-29 11:42:22 +0100

    Generate and store the API definition
    Currently, the API definition for the cameras is split across multiple
    files. This commit seeks to put them in a single file, much easier for
    generation, with the "yq" tool.
    
    It makes this easier by encoding this knowledge within the "task" tool.
    
    == Design Notes
    === Single Version
    
    This commit only supports a single version of the API, as this is all I
    can envisage working with in the near future.

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -7,16 +7,22 @@ GO     ?= $(shell which go)
 
 # Below generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
-# For example for yq variable:
+# For example for task variable:
 #
 # In your main Makefile (for non array binaries):
 #
 #include .bingo/Variables.mk # Assuming -dir was set to .bingo .
 #
-#command: $(YQ)
-#	@echo "Running yq"
-#	@$(YQ) <flags/args..>
+#command: $(TASK)
+#	@echo "Running task"
+#	@$(TASK) <flags/args..>
 #
+TASK := $(GOBIN)/task-v3.35.1
+$(TASK): $(BINGO_DIR)/task.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/task-v3.35.1"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=task.mod -o=$(GOBIN)/task-v3.35.1 "github.com/go-task/task/v3/cmd/task"
+
 YQ := $(GOBIN)/yq-v4.43.1
 $(YQ): $(BINGO_DIR)/yq.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 # BMCTL
 
-(Or, "Black Magic Control"). CLI tool & library for interacting with the Black Magic camera series, based on their 
+(Or, "Black Magic Control"). CLI tool & library for interacting with the Black Magic camera series, based on their
 OpenAPI definition.
+
+## Development
+### Tools
+
+The Go-related tools for this project are managed by [Bingo]. To install the required tools, run:
+
+```bash
+$ bingo get
+```
+
+There is a useful environment file to convert the versions into something that can be executed:
+
+```bash
+. .bingo/variables.env
+```
+
+After which the applications can be run via a variable:
+
+```bash
+$ $YQ
+Usage:
+  yq [flags]
+  yq [command]
+
+```
+
+[Bingo]: https://github.com/bwplotka/bingo
+
+
+### Task Runner
+
+The task runner is [Taskfile]. See the available tasks with:
+
+
+```bash
+$ TASK list
+```
+
+[Taskfile]: https://taskfile.dev/
