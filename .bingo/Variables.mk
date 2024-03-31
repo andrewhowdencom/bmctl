bfcea316f5582ce89e0a81bd217373ae28a3621e
Andrew Howden <hello@andrewhowden.com>
2024-03-31 10:56:59 +0200

    Move away from the generated library approach
    While trying to figure out how to generate the command line functions
    for the API, it became clear that the API code was ... pretty complex.
    It can be made a lot simpler, and more features integrated if it was
    manually written.
    
    This, of course, means there's quite a lot more work to do. I probably
    will not do most of it — This is a project that I'm using to work my
    camera. Still, this is easier for me to reason through.

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -23,21 +23,9 @@ $(COBRA_CLI): $(BINGO_DIR)/cobra-cli.mod
 	@echo "(re)installing $(GOBIN)/cobra-cli-v1.3.0"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=cobra-cli.mod -o=$(GOBIN)/cobra-cli-v1.3.0 "github.com/spf13/cobra-cli"
 
-OAPI_CODEGEN := $(GOBIN)/oapi-codegen-v2.1.0
-$(OAPI_CODEGEN): $(BINGO_DIR)/oapi-codegen.mod
-	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/oapi-codegen-v2.1.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=oapi-codegen.mod -o=$(GOBIN)/oapi-codegen-v2.1.0 "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"
-
 TASK := $(GOBIN)/task-v3.35.1
 $(TASK): $(BINGO_DIR)/task.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/task-v3.35.1"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=task.mod -o=$(GOBIN)/task-v3.35.1 "github.com/go-task/task/v3/cmd/task"
-
-YQ := $(GOBIN)/yq-v4.43.1
-$(YQ): $(BINGO_DIR)/yq.mod
-	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/yq-v4.43.1"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=yq.mod -o=$(GOBIN)/yq-v4.43.1 "github.com/mikefarah/yq/v4"
 
